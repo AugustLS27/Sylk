0e9401110d0d15c7b2ceccd6cb5ce5b8fca74ce5
August Silva <filipe.lima.silva27@gmail.com>
2023-03-04 17:22:22 +0100

    switched to vulkan over opengl for dakugen reasons
    also added a few libraries such as glm and magic_enum

--- include/sylk/window/window.hpp
+++ include/sylk/window/window.hpp
@@ -4,36 +4,48 @@
 
 #pragma once
 
+#include <vulkan/vulkan.hpp>
+
 #include <sylk/coreutils/typedefs.hpp>
 #include <sylk/data/color.hpp>
+
+#include <vector>
+#include <span>
 
 class GLFWwindow;
 
 namespace sylk {
 
-    void window_resize_callback(GLFWwindow* window, i32 width, i32 height);
-
-    struct WindowSettings {
-        const char* title = "Sylk";
-        i32 width = 1280;
-        i32 height = 720;
-        bool fullscreen = false;
-
-        i32 version_major = 3;
-        i32 version_minor = 3;
-    };
-
     class Window {
-        GLFWwindow* window;
+        struct Settings {
+            Settings();
+
+            const char* title = "Sylk";
+            i32 width = 1280;
+            i32 height = 720;
+            bool fullscreen = false;
+        };
+
+        GLFWwindow* window_;
+        Settings settings_;
+
+
+        std::vector<const char*> required_extensions_;
+
+        vk::Instance vk_instance_;
+        vk::Device vk_device_;
+        vk::PhysicalDevice vk_phys_device_;
 
     public:
-        explicit Window(WindowSettings settings = {});
+        explicit Window(Settings settings = {});
         ~Window();
 
-        void clear_color(Color color) const;
-        void clear() const;
         void poll_events() const;
         void render() const;
+
+        void create_vk_instance();
+
+        std::span<const char*> fetch_vk_required_extensions(bool force_update = false);
 
         [[nodiscard]] bool is_open() const;
     };
--- src/window/window.cpp
+++ src/window/window.cpp
@@ -1,73 +1,41 @@
 //
-// Created by August on 25-2-23.
+// Created by August Silva on 25-2-23.
 //
 
-#include <glad/glad.h>
+#include <vulkan/vulkan.hpp>
 #include <GLFW/glfw3.h>
 
 #include <sylk/window/window.hpp>
 #include <sylk/coreutils/all.hpp>
+#include <magic_enum/magic_enum.hpp>
 
 namespace sylk {
 
-    void window_resize_callback(GLFWwindow* window, i32 width, i32 height) {
-        glViewport(0, 0, width, height);
-    }
-
-    /// TODO: look into making window class static, as there can only be one window anyway.
-    /// then again, that'd be an unnecessary restriction.
-    Window::Window(const WindowSettings settings)
-        : window(nullptr) {
+    Window::Window(const Settings settings)
+        : window_(nullptr)
+        , settings_(settings)
+        , required_extensions_({}) {
         if (!glfwInit()) {
             log(CRITICAL, "GLFW initialization failed");
         }
 
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, settings.version_major);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, settings.version_minor);
-        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-        GLFWmonitor* monitor = nullptr;
+        window_ = glfwCreateWindow(settings_.width, settings_.height, settings_.title, nullptr, nullptr);
 
-        if (settings.fullscreen) {
-            monitor = glfwGetPrimaryMonitor();
-        }
-
-        window = glfwCreateWindow(settings.width, settings.height, settings.title, monitor, nullptr);
-
-        if (!window) {
-            glfwTerminate();
-            log(CRITICAL, "Failed to create window");
-        }
-
-        glfwMakeContextCurrent(window);
-
-        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-            log(CRITICAL, "Failed to initialize GLAD");
-        }
-
-        glViewport(0, 0, settings.width, settings.height);
-        glfwSetFramebufferSizeCallback(window, window_resize_callback);
-
-        clear_color({0.0f, 0.0f, 0.0f, 1.0f});
+        create_vk_instance();
     }
 
     Window::~Window() {
-        if (window) {
-            glfwDestroyWindow(window);
+        if (window_) {
+            glfwDestroyWindow(window_);
             glfwTerminate();
         }
     }
 
     bool Window::is_open() const {
-        return !glfwWindowShouldClose(window);
-    }
-
-    void Window::clear() const {
-        glClear(GL_COLOR_BUFFER_BIT);
-    }
-
-    void Window::clear_color(Color color) const {
-        glClearColor(color.r, color.g, color.b, color.a);
+        return !glfwWindowShouldClose(window_);
     }
 
     void Window::poll_events() const {
@@ -75,6 +43,51 @@ namespace sylk {
     }
 
     void Window::render() const {
-        glfwSwapBuffers(window);
     }
+
+    std::span<const char*> Window::fetch_vk_required_extensions(bool force_update) {
+        if (required_extensions_.empty() || force_update) {
+            u32 ext_count;
+            const char** instance_ext_list = glfwGetRequiredInstanceExtensions(&ext_count);
+            required_extensions_ = std::vector(instance_ext_list, instance_ext_list + ext_count);
+        }
+
+        return {required_extensions_.data(), required_extensions_.size()};
+    }
+
+    void validate(vk::Result result, const char* error_message, bool terminate = false) {
+        if (result != vk::Result::eSuccess) {
+            log((terminate ? CRITICAL : ERROR), "{} | {}", error_message, magic_enum::enum_name(result));
+        }
+    }
+
+    void Window::create_vk_instance() {
+        const vk::ApplicationInfo app_info = vk::ApplicationInfo()
+                .setPApplicationName("Sylk")
+                .setApplicationVersion(VK_MAKE_VERSION(0,1,0))
+                .setPEngineName("None")
+                .setEngineVersion(VK_MAKE_VERSION(0,1,0))
+                .setApiVersion(VK_API_VERSION_1_0);
+
+        fetch_vk_required_extensions();
+        const vk::InstanceCreateInfo instance_create_info = vk::InstanceCreateInfo()
+                .setPApplicationInfo(&app_info)
+                .setEnabledExtensionCount(required_extensions_.size())
+                .setPEnabledExtensionNames(*required_extensions_.data())
+                .setEnabledLayerCount(0);
+
+        validate(vk::createInstance(&instance_create_info, nullptr, &vk_instance_),
+                 "Failed to create Vulkan instance", true);
+
+        log(INFO, "Created Vulkan instance.");
+
+    }
+
+    // There is a GCC/Clang bug which causes an erroneous compiler error
+    // unless we default this constructor in the cpp file.
+    // The cause appears to be having nested class/struct declarations.
+    // This is not a skill issue.
+    // For more details, see https://bugs.llvm.org/show_bug.cgi?id=36684 (Clang)
+    // and https://gcc.gnu.org/bugzilla/show_bug.cgi?id=88165 (GCC)
+    Window::Settings::Settings() = default;
 }
